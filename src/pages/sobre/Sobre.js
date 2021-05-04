a0f260a24ded716dee3e22afa30cb0383e5b017f
Bcampos <bruno.leonardo.cc@gmail.com>
2021-05-04 16:45:19 -0300

    atualizaca

--- src/pages/sobre/Sobre.js
+++ src/pages/sobre/Sobre.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Sobre = () => (
     <div className="title">
         <h4>Sobre</h4>
-        <p>Aqui estão as informações a meu respeito</p>
+        <p>Aqui estão as informações</p>
     </div>
 );
 
